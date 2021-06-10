0db6c94e28066791fbaff8b18d558353e24e7414
T4r0t <85690977+T4r0t@users.noreply.github.com>
2021-06-10 16:48:27 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="#">
+    <title>Index</title>
+</head>
+<body>
+    <header>
+        <h1>Purgatory</h1>
+    </header>
+    <form action="" method="POST">
+        <label for="user">
+            Username
+        </label>
+        <input type="text" name="user" id="user" placeholder="Username">
+        <label for="pass">
+            Password
+        </label>
+        <input type="text" name="pass" id="pass" placeholder="Password">
+    </form>
+</body>
+</html>
